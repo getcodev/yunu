81f8ed79096360d7bac6a6ca9c80df0639be51aa
getcodev <getcodev@gmail.com>
2026-08-24 12:45:55 +0800

    更新了用户授权识别信息

--- config/extra/cloud.php
+++ config/extra/cloud.php
@@ -1,7 +1,7 @@
 <?php 
 return [
-	'identifier'=>'',
-	'grant'=>'',
-	'agent'=>'',
+	'identifier'=>'demo',//用户名
+	'grant'=>'bb04a5573d2bbd70d024614519b458be',
+	'agent'=>'cfcd208495d565ef66e7dff9f98764da',
 	'pid'=>'0',
 ];
